f6d667ecaf9ef100985ec65c156a1296cfb612c1
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-04-09 14:12:32 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,12 +1,12 @@
 import { endent } from '@dword-design/functions'
 
-import * as self from '.'
+import * as self from './index.js'
 
 export default {
   address: () =>
     expect(self.address).toEqual(endent`
-    Sebastian Landwehr
-    Breite Str. 20
-    53111 Bonn
-  `),
+      Sebastian Landwehr
+      Breite Str. 20
+      53111 Bonn
+    `),
 }
